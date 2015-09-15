b9c3a1c822f67d41eabd2701c028d1e860082fb4
enginekit <enginecollection@gmail.com>
2015-09-15 15:04:51 +0700

    77: IHasParameters and Its extension

---
+++ src/SharpConnect.MySql.Utils/IHasParameters.cs
@@ -0,0 +1,75 @@
+﻿//MIT 2015, brezza27, EngineKit and contributors
+
+
+namespace SharpConnect.MySql.Utils
+{
+    public interface IHasParameters
+    {
+        CommandParams Pars { get; }
+    }
+
+    public static class SimppleCommandExtensions
+    {
+        public static void AddWithValue(this IHasParameters ihasPars, string key, string value)
+        {
+            ihasPars.Pars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, byte value)
+        {
+            ihasPars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, short value)
+        {
+            ihasPars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, int value)
+        {
+            ihasPars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, long value)
+        {
+            ihasPars.Pars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, float value)
+        {
+            ihasPars.Pars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, double value)
+        {
+            ihasPars.Pars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, decimal value)
+        {
+            ihasPars.Pars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, byte[] value)
+        {
+            ihasPars.Pars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, System.DateTime value)
+        {
+            ihasPars.Pars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, sbyte value)
+        {
+            ihasPars.Pars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, char value)
+        {
+            ihasPars.Pars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, ushort value)
+        {
+            ihasPars.Pars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, uint value)
+        {
+            ihasPars.Pars.AddWithValue(key, value);
+        }
+        public static void AddWithValue(this IHasParameters ihasPars, string key, ulong value)
+        {
+            ihasPars.Pars.AddWithValue(key, value);
+        }
+
+    }
+}
---
+++ src/SharpConnect.MySql.Utils/SimpleInsert.cs
@@ -0,0 +1,74 @@
+﻿//MIT 2015, brezza27, EngineKit and contributors
+using System.Text;
+
+namespace SharpConnect.MySql.Utils
+{
+    public class SimpleInsert : IHasParameters
+    {
+        public SimpleInsert(string targetTableName)
+        {
+            TargetTableName = targetTableName;
+            Pars = new CommandParams();
+        }
+        public CommandParams Pars
+        {
+            get;
+            private set;
+        }
+        public string TargetTableName { get; private set; }
+
+        public void ExecuteNonQuery(MySqlConnection conn)
+        {
+            //create insert sql
+            //then exec
+
+            //create sql command
+
+            CommandParams pars = Pars;
+            string[] valueKeys = pars.GetAttachedValueKeys();
+
+            var stBuilder = new StringBuilder();
+            stBuilder.Append("insert into ");
+            stBuilder.Append(TargetTableName);
+            stBuilder.Append('(');
+
+            int j = valueKeys.Length;
+            for (int i = 0; i < j; ++i)
+            {
+                string k = valueKeys[i];
+                //sub string
+                if (k[0] != '?')
+                {
+                    throw new System.NotSupportedException();
+                }
+                stBuilder.Append(k.Substring(1)); //remove ?
+
+                if (i < j - 1)
+                {
+                    stBuilder.Append(',');
+                }
+
+            }
+            stBuilder.Append(")  values(");
+            for (int i = 0; i < j; ++i)
+            {
+                stBuilder.Append(valueKeys[i]);
+                if (i < j - 1)
+                {
+                    stBuilder.Append(',');
+                }
+            }
+
+
+            stBuilder.Append(")");
+
+            var sqlcmd = new MySqlCommand(stBuilder.ToString(), pars, conn);
+            sqlcmd.ExecuteNonQuery();
+
+        }
+        public void Prepare(MySqlConnection conn)
+        {
+
+        }
+    }
+}
