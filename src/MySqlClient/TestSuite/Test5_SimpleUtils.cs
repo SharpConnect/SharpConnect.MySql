d71a7a17d3bfbc38dc1a8256c72033e9c22cb920
enginekit <enginecollection@gmail.com>
2015-09-15 15:05:05 +0700

    78: TestSet5_SimpleUtils

---
+++ src/MySqlClient/TestSuite/Test5_SimpleUtils.cs
@@ -0,0 +1,44 @@
+﻿//MIT 2015, brezza27, EngineKit and contributors
+
+using System;
+using System.Collections.Generic;
+
+using SharpConnect.MySql;
+using SharpConnect.MySql.Utils;
+
+namespace MySqlTest
+{
+
+
+    public class TestSet5_SimpleUtils : MySqlTestSet
+    {
+        [Test]
+        public static void T_SimpleInsert()
+        {
+            var connStr = GetMySqlConnString();
+            var conn = new MySqlConnection(connStr);
+            conn.Open();
+
+            {
+                string sql = "drop table if exists test001";
+                var cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+
+            {
+                string sql = "create table test001(first_name varchar(100),last_name varchar(100))";
+                var cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+            //---------------------------------------------------
+            var insert = new SimpleInsert("test001");
+            insert.AddWithValue("?first_name", "test1_firstname");
+            insert.AddWithValue("?last_name", "test1_last_name");
+            insert.ExecuteNonQuery(conn);
+            //--------------------------------------------------- 
+
+            conn.Close();
+        }
+
+    }
+}
