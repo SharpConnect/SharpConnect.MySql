e1ffc3562d5fecb4b36bdbe8be1a8e45f31fbcbd
enginekit <enginecollection@gmail.com>
2016-07-16 13:47:31 +0700

    183: OpenAsync(), CloseAsync() and examples

---
+++ src/MySqlClient/TestSuite/Test7_TAP.cs
@@ -0,0 +1,71 @@
+﻿//MIT, 2015-2016, brezza92, EngineKit and contributors
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SharpConnect.MySql;
+namespace MySqlTest
+{
+    public class TestSet7_TAP : MySqlTestSet
+    {
+        [Test]
+        public static async void T_InsertAndSelect_TAP()
+        {
+
+            System.Diagnostics.Stopwatch stopW = new System.Diagnostics.Stopwatch();
+            stopW.Start();
+            //----------------------------------------
+            //1. form1: call another async method
+            await DoTaskAsync();
+            //--------------------------------------------
+            //2. form2: create task, then start and wait
+            var t2 = new Task(async () =>
+            {
+                var connStr = GetMySqlConnString();
+                var conn = new MySqlConnection(connStr);
+                conn.UseConnectionPool = true;
+                await conn.OpenAsync();
+                await conn.CloseAsync();
+            });
+            t2.Start();
+            t2.Wait();
+            //--------------------------------------------
+            //form3: use simple helper, create task and run,store task in **named** var and  wait
+            var t3 = RunTask(async () =>
+            {
+                var connStr = GetMySqlConnString();
+                var conn = new MySqlConnection(connStr);
+                conn.UseConnectionPool = true;
+                await conn.OpenAsync();
+                await conn.CloseAsync();
+            });
+            await t3;//**
+            //--------------------------------------------
+            //form4: use simple helper, create task and run, store task in **anonymous** var and wait
+            await RunTask(async () =>
+            {
+                var connStr = GetMySqlConnString();
+                var conn = new MySqlConnection(connStr);
+                conn.UseConnectionPool = true;
+                await conn.OpenAsync();
+                await conn.CloseAsync();
+            });
+            //--------------------------------------------
+            stopW.Stop();
+        } 
+        static Task RunTask(Action a)
+        {
+            var task = new Task(a);
+            task.Start();
+            return task;
+        }
+        static async Task DoTaskAsync()
+        {
+            var connStr = GetMySqlConnString();
+            var conn = new MySqlConnection(connStr);
+            conn.UseConnectionPool = true;
+            await conn.OpenAsync();
+            await conn.CloseAsync();
+        }
+    }
+}
---
+++ src/SharpConnect.MySql/0_Public_TaskAsync/MySqlConnection_A.cs
@@ -0,0 +1,31 @@
+﻿//MIT, 2016, brezza92, EngineKit and contributors
+
+using System;
+using System.Threading.Tasks;
+using SharpConnect.MySql.Internal;
+
+namespace SharpConnect.MySql
+{
+    partial class MySqlConnection
+    {
+        public Task OpenAsync()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            Open(() =>
+            {
+                tcs.SetResult(0);                
+            });
+            return tcs.Task;
+        }
+        public Task CloseAsync()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            Close(() =>
+            {
+                tcs.SetResult(0);
+            });
+            return tcs.Task;
+        }
+
+    }
+}
