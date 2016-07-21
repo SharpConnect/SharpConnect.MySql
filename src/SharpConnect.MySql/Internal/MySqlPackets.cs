ee2ddc53be45868db87ce832f71cd10a0c0978b7
enginekit <enginecollection@gmail.com>
2016-07-22 02:14:07 +0700

    209: server status flags

--- src/SharpConnect.MySql/Internal/MySqlPackets.cs
+++ src/SharpConnect.MySql/Internal/MySqlPackets.cs
@@ -72,6 +72,30 @@ namespace SharpConnect.MySql.Internal
         public const int MAX_PACKET_LENGTH = (int)(1 << 24) - 1;//(int)Math.Pow(2, 24) - 1;
     }
 
+
+    public enum MySqlServerStatus
+    {
+        //
+        //https://dev.mysql.com/doc/internals/en/status-flags.html
+        //
+        SERVER_STATUS_IN_TRANS = 0x0001, //	a transaction is active
+        SERVER_STATUS_AUTOCOMMIT = 0x0002, //	auto-commit is enabled
+        SERVER_MORE_RESULTS_EXISTS = 0x0008,
+        SERVER_STATUS_NO_GOOD_INDEX_USED = 0x0010,
+        SERVER_STATUS_NO_INDEX_USED = 0x0020,
+        SERVER_STATUS_CURSOR_EXISTS = 0x0040, //	Used by Binary Protocol Resultset to signal that COM_STMT_FETCH must be used to fetch the row-data.
+        SERVER_STATUS_LAST_ROW_SENT = 0x0080,
+        SERVER_STATUS_DB_DROPPED = 0x0100,
+        SERVER_STATUS_NO_BACKSLASH_ESCAPES = 0x0200,
+        SERVER_STATUS_METADATA_CHANGED = 0x0400,
+        SERVER_QUERY_WAS_SLOW = 0x0800,
+        SERVER_PS_OUT_PARAMS = 0x1000,
+        SERVER_STATUS_IN_TRANS_READONLY = 0x2000,//	in a read-only transaction
+        SERVER_SESSION_STATE_CHANGED = 0x4000,  //connection state information has changed 
+    }
+
+
+
     class ClientAuthenticationPacket : Packet
     {
         public uint clientFlags;
@@ -318,7 +342,7 @@ namespace SharpConnect.MySql.Internal
                 default:
                     //TODO: review here
                     throw new NotSupportedException();
-                //writer.WriteLengthCodedNull();
+                    //writer.WriteLengthCodedNull();
             }
         }
 
@@ -520,7 +544,6 @@ namespace SharpConnect.MySql.Internal
         {
             this.protocol41 = protocol41;
         }
-
         public override void ParsePacketContent(MySqlStreamReader r)
         {
             fieldCount = r.ReadByte();
@@ -756,9 +779,9 @@ namespace SharpConnect.MySql.Internal
         uint _fieldCount;
         public uint affectedRows;
         public uint insertId;
-        uint _serverStatus;
-        uint _warningCount;
-        string _message;
+        public uint _serverStatus;
+        public uint _warningCount;
+        public string _message;
         bool _protocol41;
         public OkPacket(PacketHeader header, bool protocol41)
             : base(header)
@@ -777,6 +800,10 @@ namespace SharpConnect.MySql.Internal
                 _serverStatus = r.U2();
                 _warningCount = r.U2();
             }
+
+            //TODO: review here again
+            //https://dev.mysql.com/doc/internals/en/packet-OK_Packet.html
+
             _message = r.ReadPacketTerminatedString();
             //var m = this.message.match(/\schanged:\s * (\d +) / i);
 
@@ -790,6 +817,8 @@ namespace SharpConnect.MySql.Internal
         {
             throw new NotImplementedException();
         }
+
+
     }
 
     class OkPrepareStmtPacket : Packet
@@ -1260,11 +1289,8 @@ namespace SharpConnect.MySql.Internal
                     break;
             }
         }
-
-
-
-
     }
+
 
 
 #if DEBUG
