28e29aebfadb35a0610c45d3648b3da14433777e
enginekit <enginecollection@gmail.com>
2016-07-26 12:13:48 +0700

    229: mapper experiment

---
+++ src/MySqlClient/TestSuite/Test2_SimpleMapper.cs
@@ -0,0 +1,88 @@
+﻿//MIT, 2015-2016, brezza92, EngineKit and contributors
+
+using System;
+using System.Collections.Generic;
+using SharpConnect.MySql;
+using SharpConnect.MySql.Mapper;
+namespace MySqlTest
+{
+    public class TestSet2_SimpleMapper : MySqlTestSet
+    {
+        [Test]
+        public static void T_InsertAndSelect()
+        {
+
+            try
+            {
+
+                var connStr = GetMySqlConnString();
+                var conn = new MySqlConnection(connStr);
+                conn.UseConnectionPool = true;
+                conn.Open();
+
+                DropTableIfExists(conn);
+                CreateTable(conn);
+                for (int i = 0; i < 2000; ++i)
+                {
+                    InsertData(conn);
+                }
+                SelectDataBack(conn);
+                conn.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        static void DropTableIfExists(MySqlConnection conn)
+        {
+            string sql = "drop table if exists test001";
+            var cmd = new MySqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        static void CreateTable(MySqlConnection conn)
+        {
+            string sql = "create table test001(col_id  int(10) unsigned not null auto_increment, col1 int(10)," +
+                "col2 char(2),col3 varchar(255),col4 datetime, primary key(col_id) )";
+            var cmd = new MySqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+        }
+        static void InsertData(MySqlConnection conn)
+        {
+            string sql = "insert into test001(col1,col2,col3,col4) values(10,'AA','123456789','0001-01-01')";
+            var cmd = new MySqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            uint lastInsertId = cmd.LastInsertedId;
+        }
+        static void SelectDataBack(MySqlConnection conn)
+        {
+
+
+            string sql = "select * from test001";
+            var cmd = new MySqlCommand(sql, conn);
+#if DEBUG
+            conn.dbugPleaseBreak = true;
+#endif
+            //this is very basic mapper***
+            var reader = cmd.ExecuteReader();
+            //------------------------------------------------------
+
+            var mapper = Mapper.Map((object target, int col_id) =>
+            {
+
+            });
+            //------------------------------------------------------
+            //create sample record
+            //var targetRecordSample = new { col_id = 0, col1 = 0, col2 = "", col3 = "", colo4 = DateTime.MinValue };
+            while (reader.Read())
+            {
+                //simple map query result to member of the target object
+                //reader.NewRecordLike(targetRecordSample);
+                 
+            }
+            reader.Close();
+        }
+    }
+}
---
+++ src/SharpConnect.MySql/1_Public_ResultMapper/ResultMapperExtension.cs
@@ -0,0 +1,125 @@
+﻿//MIT, 2015-2016, brezza92, EngineKit and contributors
+
+using System;
+using System.Collections.Generic;
+using SharpConnect.MySql.Internal;
+using System.Text;
+using System.Reflection;
+using SharpConnect;
+namespace SharpConnect.MySql.Mapper
+{
+    public class MapRecord<R, T>
+    {
+        MapAction<R, T> recordMapDel;
+        public MapRecord(MapAction<R, T> recordMapDel)
+        {
+            this.recordMapDel = recordMapDel;
+
+        }
+        public T M1 { get; set; }
+        public string GetFieldListString()
+        {
+            //----------------------------------------
+            //create simple selection list
+            //TODO: check if we have create the string list or not 
+            var originalMethod = recordMapDel.GetMethodInfo();
+
+            //get paraemter list
+            var parInfos = originalMethod.GetParameters();
+            int j = parInfos.Length;
+            StringBuilder stbuilder = new StringBuilder();
+            for (int i = 1; i < j; ++i)
+            {
+                //start from 1***
+                var parInfo = parInfos[i];
+                if (i > 1)
+                {
+                    stbuilder.Append(',');
+                }
+                stbuilder.Append(parInfo.Name);
+            }
+            return stbuilder.ToString();
+        }
+    }
+
+    public abstract class MapRecordBase
+    {
+        public abstract string GetFieldListString();
+    }
+    public class MapRecord2<T> : MapRecordBase
+    {
+        MapNew<T> recordMapDel;
+        public MapRecord2(MapNew<T> recordMapDel)
+        {
+            this.recordMapDel = recordMapDel;
+
+        }
+        public T M1 { get; set; }
+        public override string GetFieldListString()
+        {
+            //----------------------------------------
+            //create simple selection list
+            //TODO: check if we have create the string list or not
+
+            var originalMethod = recordMapDel.GetMethodInfo();
+            //get paraemter list
+            var parInfos = originalMethod.GetParameters();
+            int j = parInfos.Length;
+            StringBuilder stbuilder = new StringBuilder();
+            for (int i = 0; i < j; ++i)
+            {
+                //start from 0***
+                var parInfo = parInfos[i];
+                if (i > 0)
+                {
+                    stbuilder.Append(',');
+                }
+                stbuilder.Append(parInfo.Name);
+            }
+            return stbuilder.ToString();
+        }
+    }
+
+
+
+    public class MapRecord<R, T1, T2>
+    {
+        Action<R, T1, T2> recordMapDel;
+        public MapRecord(Action<R, T1, T2> recordMapDel)
+        {
+            this.recordMapDel = recordMapDel;
+        }
+        public T1 M1 { get; set; }
+        public T2 M2 { get; set; }
+    }
+
+    public delegate void MapAction<R, T>(R r, T t);
+    public delegate void MapNew<T>(T t);
+    public static class Mapper
+    {
+        public static MapRecord2<T> New<T>(MapNew<T> ac)
+        {
+            return new MapRecord2<T>(ac);
+        }
+
+        //----------------------------------------------------------------------
+        public static MapRecord<R, T> Map<R, T>(MapAction<R, T> ac)
+        {
+            return new MapRecord<R, T>(ac);
+        }
+        public static MapRecord<R, T1, T2> Map<R, T1, T2>(Action<R, T1, T2> ac)
+        {
+            return new MapRecord<R, T1, T2>(ac);
+        }
+    }
+    public static class SqlBuild
+    {
+        public static string Select<T>(string srcName, MapRecord2<T> record)
+        {
+            //create query string from record
+            return "select " +
+             record.GetFieldListString() +
+             "from " + srcName;
+        }
+    }
+}
